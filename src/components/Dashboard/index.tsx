5bc76b5ccccaceb1a1239021a22913e99d74e89e
adriano25klein@gmail.com <you@example.com>
2022-01-19 17:51:34 -0300

    PRojeto inicial

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import { Summary } from "../Summary/index";
+import { TransactionTable } from "../TransactionTable";
+import { Container } from "./styles";
+
+export function Dashboard() {
+  return (
+    <>
+      <Container>
+        <Summary />
+        <TransactionTable />
+      </Container>
+    </>
+  );
+}
