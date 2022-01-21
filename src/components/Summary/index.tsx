bb2a43b1145bd20a225a40c5f8815ba1536e11c1
adriano25klein@gmail.com <you@example.com>
2022-01-21 12:34:46 -0300

    Criando o hook de criação de transação

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,10 +3,10 @@ import { Container } from "../Summary/styles";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useTransactions();
 
   const summary = transactions.reduce(
     (acc, transaction) => {
@@ -47,6 +47,7 @@ export function Summary() {
           <img src={outcomeImg} alt="Entradas" />
         </header>
         <strong>
+          -
           {new Intl.NumberFormat("pt-BR", {
             style: "currency",
             currency: "BRL",
